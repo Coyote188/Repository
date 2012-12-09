6d4ac019b1429f4bfe5d3821d1cb612772016afa
Coyote <ligang.cui@gmail.com>
2012-12-09 21:03:36 +0800

    Signed-off-by: Coyote <ligang.cui@gmail.com>

--- src/org/test/RepositoryTest.java
+++ src/org/test/RepositoryTest.java
@@ -9,13 +9,12 @@ public class RepositoryTest {
 
 	@Test
 	public void test() {
-		ObjectAnalytic object = new ObjectAnalytic();
 		TestBean bean = new TestBean();
 		bean.setCreateDate(new java.sql.Timestamp(System.currentTimeMillis()));
 		bean.setUserArea("0871");
 		bean.setUserNum("15398577080");
 		
-		object.analitic(bean);
+		ObjectAnalytic.analitic(bean);
 	}
 	
 	@Test
